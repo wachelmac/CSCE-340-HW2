ab39fcbe3da843ad9bc51d43e0b26db8765f7d07
wachelmac <78569827+wachelmac@users.noreply.github.com>
2021-02-27 12:40:02 -0600

    Look a prototype print function

--- WorstFit.java
+++ WorstFit.java
@@ -1,1 +1,20 @@
+//Rachel McMullan
+//George O'Malley
 
+public class WorstFit {
+    
+    int[] files=new int[20];
+    double sum=0;
+    int numDisks=0;
+    
+    private void sumFiles(){
+        
+    }
+    
+    public void printFit(){
+        System.out.printf("file sizes sum\t = %f GB\n", sum);
+        System.out.printf("total disks\t = %d\n", numDisks);
+        
+    }
+    
+}
