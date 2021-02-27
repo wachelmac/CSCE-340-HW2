a5ef1ea34b73c84c0bffee252c21d7601e3151ed
wachelmac <78569827+wachelmac@users.noreply.github.com>
2021-02-27 13:55:48 -0600

    Everything but Kitchen Sink

--- WorstFit.java
+++ WorstFit.java
@@ -2,24 +2,51 @@
 //George O'Malley
 
 import java.util.ArrayList;
-import java.util.Collections;
-
+//import java.util.Collections;
 
 public class WorstFit {
     
     ArrayList<Disk> disks=new ArrayList<Disk>();
-    int[] files=new int[20];
-    double sum=0;
-    int numDisks=0;
+    int[] files;
+    int numDisks;
+    int index;
     
-    private void sumFiles(){
+    WorstFit(int[] f){
         
+        files=f;
+        addDisk();
+        doWorst();
     }
     
-    public void printFit(){
-        System.out.printf("file sizes sum\t = %f GB\n", sum);
-        System.out.printf("total disks\t = %d\n", numDisks);
+    private void addDisk(){
+        Disk nextDisk=new Disk(numDisks);
+        disks.add(numDisks, nextDisk);
+        numDisks+=1;
+    }
+    
+    private void doWorst(){
         
+        for(int i=0; i<files.length; i++){
+            //ALGORITHM GOES HERE
+        }
+    }
+    
+    private double sumFiles(){
+        int total=0;
+        for(int i=0; i<files.length; i++)
+            total+=files[i];
+        return (double)total/1000000;
+    }
+    
+    
+    public void printFit(){
+        
+        System.out.printf("file sizes sum\t = %f GB\n", sumFiles());
+        System.out.printf("total disks\t = %d\n", numDisks);
+        if(files.length<100){
+            for(Disk d: disks)
+                d.printDisk();
+            }
     }
     
 }
