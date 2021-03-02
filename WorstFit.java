a67a33f3021fba8dd33b4506c4dd4b45663d5c6c
wachelmac <78569827+wachelmac@users.noreply.github.com>
2021-03-01 18:45:13 -0600

    Update WorstFit.java

--- WorstFit.java
+++ WorstFit.java
@@ -2,6 +2,7 @@
 //George O'Malley
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class WorstFit {
     
@@ -16,35 +17,32 @@ public class WorstFit {
     
     private void addDisk(){
         Disk nextDisk=new Disk(numDisks);
-        disks.add(numDisks, nextDisk);
+        disks.add(nextDisk);
         numDisks+=1;
+        
     }
     
     private void doWorst(){
         addDisk();
-        Disk d; 
         int fileSize;//holds current value
+        MaxPQ<Disk> pq;
         
         for(int i=0; i<files.length; i++){
             fileSize=files[i];
             boolean isSpace=false; 
-
-            for(int j=0; j<numDisks; j++){
-                d=disks.get(j);
+            for(Disk d: disks){
                 if(!d.isFull(fileSize)){
                     isSpace=true; 
                     break; 
                 }
             }
             if(!isSpace){
-                System.out.println("we need a new disk");
                     addDisk();//create new disk
-                    assert disks.get(numDisks-1).addFile(fileSize);//add file to disk
+                    disks.get(numDisks-1).addFile(fileSize);//add file to disk
             }  
             else{
-                MaxPQ<Disk> pq=new MaxPQ(disks.toArray());//create a priority queue 
-                Disk max= pq.max(); //find the disk with the most remaining space
-                max.addFile(fileSize);
+                pq=new MaxPQ(disks.toArray());//create a priority queue 
+                pq.max().addFile(fileSize);
             }
         }
         printFit();
@@ -63,9 +61,11 @@ public class WorstFit {
         System.out.printf("file sizes sum\t = %f GB\n", sumFiles());
         System.out.printf("total disks\t = %d\n", numDisks);
         if(files.length<100){
+            //sort goes here
+            
             for(Disk d: disks)
                 d.printDisk();
-            }
+        }
     }
     
 }
