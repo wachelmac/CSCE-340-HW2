17ebcb2e2fcaed858c8056ab17785848c0f29ba9
wachelmac <78569827+wachelmac@users.noreply.github.com>
2021-03-03 11:08:23 -0600

    Final Draft (hopefully)

--- WorstFit.java
+++ WorstFit.java
@@ -1,31 +1,34 @@
 //Rachel McMullan
 //George O'Malley
+//Group 7
 
 import java.util.ArrayList;
 import java.util.Comparator;
+import java.io.*;
+import java.util.Scanner;
 
 public class WorstFit {
     
-    ArrayList<Disk> disks=new ArrayList<Disk>();
-    int[] files;
+    ArrayList<Disk> disks=new ArrayList<>();
+    int[] files; 
     int numDisks;
     
-    WorstFit(int[] f){
-        files=f;
+    
+    WorstFit(String s) throws IOException{
+        
+        //process data from file 
+        readFile(s);
+        
+        //implement worst-fit heuristic 
         doWorst();
     }
     
-    private void addDisk(){
-        Disk nextDisk=new Disk(numDisks);
-        disks.add(nextDisk);
-        numDisks+=1;
-        
-    }
-    
     private void doWorst(){
-        addDisk();
-        int fileSize;//holds current value
+        
+        addDisk();//create first disk 
+        int fileSize;
         MaxPQ<Disk> pq;
+        
         
         for(int i=0; i<files.length; i++){
             fileSize=files[i];
@@ -48,6 +51,34 @@ public class WorstFit {
         printFit();
     }
     
+    //reads input from file and stores it in integer array files[]
+    private void readFile(String s) throws IOException{
+               
+        File inFile=new File(s);
+        Scanner input=new Scanner(inFile);
+        ArrayList<Integer> inList=new ArrayList<>();
+        
+        while(input.hasNext()){
+          inList.add(input.nextInt());  
+        }
+        
+        //copy file sizes to files[] for easier access 
+        int size=inList.size();
+        files=new int[size];
+        for(int i=0; i<size; i++){
+            files[i]=inList.get(i);
+        }
+    }
+    
+    //adds disk to ArrayList disks and updates total
+    private void addDisk(){
+        Disk nextDisk=new Disk(numDisks);
+        disks.add(nextDisk);
+        numDisks+=1;
+        
+    }
+    
+    //returns the sum of all file sizes in GB 
     private double sumFiles(){
         int total=0;
         for(int i=0; i<files.length; i++)
@@ -56,15 +87,17 @@ public class WorstFit {
     }
     
     
+    //prints the sum of all fizes and the total number of disks
     public void printFit(){
-        
         System.out.printf("file sizes sum\t = %f GB\n", sumFiles());
         System.out.printf("total disks\t = %d\n", numDisks);
+        
+        //if there are less than 100 files, print disks
         if(files.length<100){
-            //sort goes here
-            
+            MaxPQ<Disk> pq=new MaxPQ(disks.toArray());
+
             for(Disk d: disks)
-                d.printDisk();
+                pq.delMax().printDisk();
         }
     }
     
