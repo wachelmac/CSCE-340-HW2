a99b005dae160ef8dff8fee4205900d3d3fe3f03
wachelmac <78569827+wachelmac@users.noreply.github.com>
2021-03-04 12:38:29 -0600

    Plus comments

--- Disk.java
+++ Disk.java
@@ -1,21 +1,24 @@
 //Rachel McMullan
 //George O'Malley
+//Group 7
+
 import java.util.ArrayList;
 
 public class Disk implements Comparable<Disk> {
   
     final int SIZE=1000000;//each disk has a capacity of 1GB or 1,000,000KB
-    private int space; 
-    private int id; 
+    private int space; //remaining space on disk, Comparable variable 
+    private int id; //unique id associated with each disk
     ArrayList files=new ArrayList();//files stored on the disk 
     
     Disk(int id){
-        //inUse=0;//upon creation, the disk is empty
+        //constructor initializes Disk object
         space=SIZE; 
         this.id=id;
     }
-	
+
     public int compareTo(Disk d){  
+        //Comparable method compares remaining space on disks 
         if(this.space==d.space)  
             return 0;  
         else if(this.space>d.space)  
@@ -23,8 +26,8 @@ public class Disk implements Comparable<Disk> {
         else  
             return -1;  
     }
-  
     public boolean isFull(int f){
+        //if false, there is no space on the disk for the file 
         return (space-f)< 0;
     }
     
@@ -34,11 +37,11 @@ public class Disk implements Comparable<Disk> {
             return false;
         //if there is space, update space and add the new file to the list
         space-=f;
-        
         return files.add(f);  
     }
-    
+
     public void printDisk(){
+        //output disk metrics in the given format 
         System.out.printf("\t %d %6d:  ", id, space);
         for(int i=0; i<files.size(); i++)
             System.out.print(files.get(i)+" ");
